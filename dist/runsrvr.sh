a29e499cf160767cffd4dc97a78ca1a1e492de31
suri <suri980702@gmail.com>
2020-11-24 10:54:01 -0500

    scripts

---
+++ dist/runsrvr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -z "$ZOOBINDIR" ]]
+then
+	echo "Error!! ZOOBINDIR is not set" 1>&2
+	exit 1
+fi
+
+. $ZOOBINDIR/zkEnv.sh
+
+#TODO Include your ZooKeeper connection string here. Make sure there are no spaces.
+# 	Replace with your server names and client ports.
+export ZKSERVER=lab2-21.cs.mcgill.ca:21821,lab2-20.cs.mcgill.ca:21821,lab2-19.cs.mcgill.ca:21821
+
+java -cp $CLASSPATH:../task:.: DistProcess 
